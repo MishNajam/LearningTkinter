4a481970f37301e754ceae64e6d6ceda6c0fdda8
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2022-07-15 16:02:45 +0100

    Configure labels and buttons

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,15 @@ import tkinter as tk
 
 def open_window():
     window = tk.Tk()
-    greeting = tk.Label(text="Hello, Tkinter")
-    greeting.pack()
+
+    # height and width are measured in text units and so are uneven
+    label = tk.Label(text="Hello, Tkinter", fg="white", bg="black", width=10, height=10)
+    label.pack()
+
+    button = tk.Button(text="Click me!", width=25, height=5, bg="blue", fg="yellow")
+    button.pack()
+
+    entry = tk.Entry(fg="yellow", bg="blue", width=50)
+    entry.pack()
+
     window.mainloop()
